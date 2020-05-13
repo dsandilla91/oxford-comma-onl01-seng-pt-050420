8b77b70573da964141c255d84e88226f61320b5c
Deborah Santillan <s.debb@yahoo.com>
2020-05-13 20:19:38 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Oxford Comma List Formatter
+till # Oxford Comma List Formatter
 
 ## Objectives
 
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
+  if array.length == 1 
+    array.join 
+    
+  elsif array.length == 2
+    array.join(" and ")  
+    
+  elsif array.length >= 3 
+     array.join(', ')
+       
+  end 
+end 
+ 
+# [sam]
+# sam 
 
-end
+# [sam, john]
+# "sam, john"
+
+# sam john mary
+# sam, john, and mary
+
+
+
+
+# return array.join(", ")
+

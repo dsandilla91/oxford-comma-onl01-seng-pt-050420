d5e0aab8f7a26c5fdbe68d5ae53640eb42a94006
Deborah Santillan <s.debb@yahoo.com>
2020-05-13 20:36:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
+  if array.length == 1 
+    array.join 
+    
+  elsif array.length == 2
+    array.join(" and ")  
+    
+  elsif array.length >= 3 
+      array[-1].insert(0, "and ")
+      array.join(', ')
+       
+  end 
+end 
+ 
+# [sam]
+# sam 
 
-end
+# [sam, john]
+# "sam, john"
+
+# sam john mary
+# sam, john, and mary
+
+
+
+
+# return array.join(", ")
+
